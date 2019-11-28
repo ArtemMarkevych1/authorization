e7c62b6fabf567265d115d9ce8ec0fe954d325f6
ArtemMarkevych1 <aw.markevich@gmail.com>
2019-11-28 17:11:34 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -9,40 +9,76 @@ class App extends Component {
       { name: "Mazda", year: 2017 },
       { name: "BMW", year: 2016 }
     ],
-    pageTitle: "There are the cars"
+    pageTitle: "Cars list", // any custom value via string
+    showCars: false
   };
 
-  handleClick = () => {
-    const newTitle = this.state.pageTitle // создаем новую переменную, которая будет делать что то со старым стейтом
-      .split("")
-      .reverse()
-      .join("");
-
+  toggleCarsHandler = () => {
     this.setState({
-      pageTitle: newTitle
+      showCars: !this.state.showCars
     });
+  };
 
-    // const newTitle = this.state.pageTitle
-    //   .split("")
-    //   .reverse()
-    //   .join("");
+  handleClick = newTitle => {
+    this.setState({
+      pageTitle: newTitle // переназначаем pageTitle
+    });
+  };
 
-    // this.setState({
-    //   pageTitle: newTitle
-    // });
-    // console.log('Click was')
+  handleInput = event => {
+    this.setState({
+      pageTitle: event.target.value //
+    });
+  };
+
+  divStyle = {
+    textAlign: "center"
   };
 
   render() {
-    const carse = this.state.cars;
+    // const cars = this.state.cars;
 
     return (
-      <div>
-        <h1>{this.state.pageTitle}</h1>
-        <button onClick={this.handleClick}>Change title</button>
-        <Car name={carse[0].name} year={carse[0].year} />
-        <Car name={carse[1].name} year={carse[1].year} />
-        <Car name={carse[2].name} year={carse[2].year} />
+      <div style={this.divStyle}>
+        <h1 style={{ color: "red", fontSize: "45px" }}>
+          {this.state.pageTitle}
+        </h1>
+        <button onClick={this.toggleCarsHandler}>Toggle cars</button>
+        <br />
+        <br />
+        <input type="text" onChange={this.handleInput}></input>
+        <br />
+        <br />
+        <button onClick={this.handleClick.bind(this, "Title is changed")}>
+          Change Title
+        </button>
+        {this.state.showCars
+          ? this.state.cars.map((car, index) => {
+              return (
+                <Car
+                  key={index}
+                  name={car.name}
+                  year={car.year}
+                  onChangeTitle={() => this.handleClick(car.name)}
+                />
+              );
+            })
+          : null}
+        {/* <Car
+          name={cars[0].name}
+          year={cars[0].year}
+          onChangeTitle={this.handleClick.bind(this, cars[0].name)}
+        ></Car>
+        <Car
+          name={cars[1].name}
+          year={cars[1].year}
+          onChangeTitle={() => this.handleClick(cars[1].name)}
+        ></Car>
+        <Car
+          name={cars[2].name}
+          year={cars[2].year}
+          onChangeTitle={this.handleClick.bind(this, cars[2].name)}
+        ></Car> */}
       </div>
     );
   }
--- src/components/Car/Car.js
+++ src/components/Car/Car.js
@@ -1,12 +1,19 @@
 import React from "react";
 
-export default ({ name, year, children }) => (
+export default props => (
   <div>
-    <h3>Car name: {name}</h3>
-    <p>
-      Number: <strong>{year}</strong>
-    </p>
-    {children}
-
+    <h3>Car name: {props.name}</h3>
+    <h3>Year: {props.year}</h3>
+    <button onClick={props.onChangeTitle}>Click here</button>
   </div>
 );
+
+// export default props => (
+//   <div>
+//     <h3>Car name: {props.name}</h3>
+//     <p>
+//       Number: <strong>{props.year}</strong>
+//     </p>
+//     <button onClick={props.onChangeTitle}>Click</button>
+//   </div>
+// );
