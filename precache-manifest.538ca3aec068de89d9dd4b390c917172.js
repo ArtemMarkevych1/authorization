02934b0edcf133c58f52441eab664f6aad95b9b4
ArtemMarkevych1 <aw.markevich@gmail.com>
2019-12-01 15:09:28 +0200

    Updates

---
+++ precache-manifest.538ca3aec068de89d9dd4b390c917172.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "609d55f5d0589516b3d1b64439860a7e",
+    "url": "/authorization/index.html"
+  },
+  {
+    "revision": "3adaee0dc7f7d4e8bae9",
+    "url": "/authorization/static/css/main.d43202b2.chunk.css"
+  },
+  {
+    "revision": "0899533824abdbfd3935",
+    "url": "/authorization/static/js/2.50609584.chunk.js"
+  },
+  {
+    "revision": "3adaee0dc7f7d4e8bae9",
+    "url": "/authorization/static/js/main.1eb98739.chunk.js"
+  },
+  {
+    "revision": "51cf543ec9c5805c8afc",
+    "url": "/authorization/static/js/runtime-main.e16bf57c.js"
+  }
+]);
